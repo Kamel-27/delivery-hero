f7803d4211a672dae595dcb862bfe2a05f2ff95e
Kamel-27 <119250341+Kamel-27@users.noreply.github.com>
2026-06-17 18:43:41 +0300

    feat: implement auth registration route and controller with request validation

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
 import { healthRouter } from "./app/health/health.routes.js";
+import { authRouter } from "./app/auth/routes.js";
 export const router = Router();
 router.use("/health", healthRouter);
+router.use('/auth', authRouter);
